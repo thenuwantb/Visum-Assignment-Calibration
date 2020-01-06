898742940efe72497677058d6bda28b3ffadabe7
thenuwan.jayasinghe <thenuwan.jayasinghe@CE-2013-126.TUMCREATE.LOCAL>
2020-01-06 10:55:54 +0800

    Script for creation of a network with Visum (Zones, Nodes, Connectors, Links and Stops)

---
+++ visum_calibration/visum_network_creation/network_2.py
@@ -0,0 +1,113 @@
+'''
+Created on 6 Jan 2020
+
+@author: thenuwan.jayasinghe
+'''
+import sys
+import os.path
+import win32com.client as com
+import datetime 
+import pandas as pd
+import numpy as np
+import timeit
+
+#Load Visum Version and Create a Network Object
+path = "C:\\Users\\thenuwan.jayasinghe\\Documents\\_Thesis\\Coding\\02_Network"
+verFile = "1_Blank.ver"
+versionPath =  os.path.join(path, verFile)
+Visum = com.Dispatch("Visum.Visum.180")
+Visum.LoadVersion(versionPath)
+
+Net = Visum.Net
+
+linktype20 = Net.AddLinkType(20)
+linktype20.SetAttValue("TSYSSET", "B")
+
+
+#The aim is to add Zones, nodes, links and connectors. Rest of the changes area done within Visum itself
+
+#Add Zones
+zoneData =  pd.read_csv(os.path.join(path, "Zones.csv"))
+for index, row in zoneData.iterrows():
+    
+    no = row['no']
+    name = row['name']
+    XCoord = row['XCoord']
+    YCoord = row['YCoord']
+    
+    zone = Net.AddZone(no)
+    zone.SetAttValue("NAME", str(name))
+    zone.SetAttValue("XCOORD", str(XCoord))
+    zone.SetAttValue("YCOORD", str(YCoord))
+
+#Add Nodes
+nodeData = pd.read_csv(os.path.join(path, "Nodes.csv"))
+for index, row in nodeData.iterrows():
+    no = row['no']
+    xCoord = row['XCoord']
+    yCoord = row['YCoord']
+     
+    Net.AddNode(no, xCoord, yCoord)
+
+#Add Connectors
+connectorsData = pd.read_csv(os.path.join(path, "Connectors.csv"))
+for index, row in connectorsData.iterrows():
+    
+    zone = int(row['zone'])
+    node = int(row['node'])
+    
+    Net.AddConnector(zone, node)
+    
+#Add Links
+
+linkData = pd.read_csv(os.path.join(path, "Links.csv"))
+for index, row in linkData.iterrows():
+    fromNode = int(row['fromNode'])
+    toNode = int(row['toNode'])
+    linkType = int(row['linkType'])
+    
+    Net.AddLink(-1, fromNode, toNode, linkType)
+    
+#Add Stop related data
+stopFilePath = os.path.join(path, "Stops.xlsx")
+
+print stopFilePath
+stopData = pd.read_excel(stopFilePath, 'stops')
+stopAreaData = pd.read_excel(stopFilePath, 'stopAreas')
+stopPointsOnNodeData = pd.read_excel(stopFilePath, 'stopPointsOnNodes')
+
+##Add Stops
+for index, row in stopData.iterrows():
+    no = int(row['no'])
+    stopName = str(row['stopName'])
+    XCoord = int(row['XCoord'])
+    YCoord = int(row['YCoord'])
+    
+    stop = Net.AddStop(no, XCoord, YCoord)
+    stop.SetAttValue("NAME", stopName)
+    
+##Add Stop Areas
+for index, row in stopAreaData.iterrows():
+    no = int(row['no'])
+    stop = int(row['stop'])
+    node = int(row['node'])
+    xCoord = int(row['XCoord'])
+    yCoord = int(row['YCoord'])
+    
+    Net.AddStopArea(no, stop, node, xCoord, yCoord)
+    
+##Add Stop Points on Node
+for index, row in stopPointsOnNodeData.iterrows():
+    no = int(row['no'])
+    stopArea = int(row['stopArea'])
+    node = int(row['node'])
+    
+    Net.AddStopPointOnNode(no, stopArea, node)
+
+#-------------------------End Main Code-----------------------------------------------------
+
+#Write the new Version file
+fileName, ext = os.path.splitext(versionPath)
+saveAs = "Network_2" + "_" + datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d-%H%M") + ext
+saveFileTo = os.path.join(os.path.dirname(versionPath), saveAs)
+Visum.SaveVersion(saveFileTo)
