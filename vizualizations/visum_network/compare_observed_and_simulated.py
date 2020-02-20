770d787e24743f17195c46c611e9bbba398bb44b
thenuwan.jayasinghe <thenuwan.jayasinghe@CE-2013-126.TUMCREATE.LOCAL>
2020-02-20 11:22:17 +0800

    commit - 20022020

---
+++ vizualizations/visum_network/compare_observed_and_simulated.py
@@ -0,0 +1,21 @@
+'''
+Created on 19 Feb 2020
+
+@author: thenuwan.jayasinghe
+@note : I wanted to make sure that the Direct assignment model and the headway-based simulation model are in same temporal scale, so that it is made sure that the error term calculated is make sense
+'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_df = pd.read_excel("C:\\Users\\thenuwan.jayasinghe\\OneDrive - tum.de\\Thesis\\3_Analysis\\Manual Calibration\\Stops Data\\Stops\\stops_AP_level_analysis_19022020_hist.xlsx")
+
+#===============================================================================
+# Index([u'NO', u'CODE', u'NAME', u'PASSORIGIN(AP)_1', u'PASSDESTINATION(AP)_1',
+#        u'PASSORIGIN(AP)_10', u'PASSDESTINATION(AP)_10'],
+#       dtype='object')
+#===============================================================================
+
+data_df.boxplot(column = ['PASSORIGIN(AP)_1','PASSDESTINATION(AP)_1'] , showfliers=False)
+#data_df.boxplot(column = ['PASSORIGIN(AP)_10','PASSDESTINATION(AP)_10'] , showfliers=False)
+plt.show()
--- vizualizations/visum_network/observed_route_share_sample.py
+++ vizualizations/visum_network/observed_route_share_sample.py
@@ -8,70 +8,69 @@ Created on 17 Feb 2020
 import pandas as pd
 import matplotlib.pyplot as plt
 
+col_names  = ['ORIGZONENO','DESTZONENO','INDEX','ODTRIPS','NUMTRANSFERS','INVEHDIST']
+path = "C:\\Users\\thenuwan.jayasinghe\\OneDrive - tum.de\\Thesis\\3_Analysis\\Direct Assignment\\sampel_PuT_Paths_O3004925_17022020.csv"
+data = pd.read_csv(path, header = None, names = col_names)
+ 
+filter_1 = data['DESTZONENO'] == 3003190
+#print data[filter_1]
 #===============================================================================
-# col_names  = ['ORIGZONENO','DESTZONENO','INDEX','ODTRIPS','NUMTRANSFERS','INVEHDIST']
-# data = pd.read_csv("C:\\Users\\thenuwan.jayasinghe\\OneDrive - tum.de\\Thesis\\3_Analysis\\Direct Assignment\\sampel_PuT_Paths_O3004925_17022020.csv", header = None, names = col_names)
-# 
-# filter_1 = data['DESTZONENO'] == 3003190
-# #print data[filter_1]
-# #===============================================================================
-# #     ORIGZONENO  DESTZONENO  INDEX  ODTRIPS  NUMTRANSFERS  INVEHDIST
-# # 0      3004925     3003190      1      1.0           1.0      22.97
-# # 1      3004925     3003190      2      1.0           3.0      24.43
-# # 2      3004925     3003190      3      1.0           1.0      22.97
-# # 3      3004925     3003190      4      1.0           1.0      22.97
-# # 4      3004925     3003190      5      1.0           1.0      30.18
-# # 5      3004925     3003190      6      1.0           1.0      22.63
-# # 6      3004925     3003190      7      1.0           1.0      22.63
-# # 7      3004925     3003190      8      1.0           1.0      22.97
-# # 8      3004925     3003190      9      1.0           1.0      22.97
-# # 9      3004925     3003190     10      1.0           1.0      22.63
-# # 10     3004925     3003190     11      1.0           1.0      22.97
-# #===============================================================================
-# #sample1 =data.groupby(['ORIGZONENO', 'DESTZONENO'])['ODTRIPS'].sum().to_frame().reset_index() #works
-# 
-# unique_route_sum = data.groupby(['ORIGZONENO', 'DESTZONENO', 'NUMTRANSFERS', 'INVEHDIST'])['ODTRIPS'].sum().to_frame().reset_index()
-# 
-# filter_2 = unique_route_sum['DESTZONENO'] == 3003190
-# 
-# 
-# #print unique_route_sum[filter_2]
-# #===============================================================================
-# #    ORIGZONENO  DESTZONENO  NUMTRANSFERS  INVEHDIST  ODTRIPS
-# # 0     3004925     3003190           1.0      22.63      3.0
-# # 1     3004925     3003190           1.0      22.97      6.0
-# # 2     3004925     3003190           1.0      30.18      1.0
-# # 3     3004925     3003190           3.0      24.43      1.0
-# #===============================================================================
-# 
-# trip_sum_per_odpair = data.groupby(['ORIGZONENO', 'DESTZONENO'])['ODTRIPS'].sum().to_frame().reset_index()
-# filter_3 = trip_sum_per_odpair['DESTZONENO'] == 3003190
-# 
-# #print trip_sum_per_odpair[filter_3]
-# 
-# #===============================================================================
-# #    ORIGZONENO  DESTZONENO  ODTRIPS
-# # 0     3004925     3003190     11.0
-# #===============================================================================
-# 
-# total_merged_df = unique_route_sum.merge(trip_sum_per_odpair, on = ['ORIGZONENO', 'DESTZONENO'])
-# change_col_total_merged = {'ODTRIPS_x' : 'ROUTE_OD',  'ODTRIPS_y':'TOTAL_OD' }
-# total_merged_df = total_merged_df.rename(columns = change_col_total_merged)
-# 
-# total_merged_df['SHARE(%)'] = (total_merged_df['ROUTE_OD'] / total_merged_df['TOTAL_OD'])*100
-# 
-# 
-# filter_4 = total_merged_df['DESTZONENO'] == 3003190
-# 
-# print total_merged_df[filter_4]
-# 
-# save_resuls = "C:\\Users\\thenuwan.jayasinghe\\OneDrive - tum.de\\Thesis\\3_Analysis\\Direct Assignment\\sample_results_18022020.csv"
-#  
-# total_merged_df.to_csv(save_resuls)
-#  
-# total_merged_df.hist(column = 'SHARE(%)', bins = 100)
-# plt.show()
+#     ORIGZONENO  DESTZONENO  INDEX  ODTRIPS  NUMTRANSFERS  INVEHDIST
+# 0      3004925     3003190      1      1.0           1.0      22.97
+# 1      3004925     3003190      2      1.0           3.0      24.43
+# 2      3004925     3003190      3      1.0           1.0      22.97
+# 3      3004925     3003190      4      1.0           1.0      22.97
+# 4      3004925     3003190      5      1.0           1.0      30.18
+# 5      3004925     3003190      6      1.0           1.0      22.63
+# 6      3004925     3003190      7      1.0           1.0      22.63
+# 7      3004925     3003190      8      1.0           1.0      22.97
+# 8      3004925     3003190      9      1.0           1.0      22.97
+# 9      3004925     3003190     10      1.0           1.0      22.63
+# 10     3004925     3003190     11      1.0           1.0      22.97
 #===============================================================================
+#sample1 =data.groupby(['ORIGZONENO', 'DESTZONENO'])['ODTRIPS'].sum().to_frame().reset_index() #works
+ 
+unique_route_sum = data.groupby(['ORIGZONENO', 'DESTZONENO', 'NUMTRANSFERS', 'INVEHDIST'])['ODTRIPS'].sum().to_frame().reset_index()
+ 
+filter_2 = unique_route_sum['DESTZONENO'] == 3003190
+ 
+ 
+#print unique_route_sum[filter_2]
+#===============================================================================
+#    ORIGZONENO  DESTZONENO  NUMTRANSFERS  INVEHDIST  ODTRIPS
+# 0     3004925     3003190           1.0      22.63      3.0
+# 1     3004925     3003190           1.0      22.97      6.0
+# 2     3004925     3003190           1.0      30.18      1.0
+# 3     3004925     3003190           3.0      24.43      1.0
+#===============================================================================
+ 
+trip_sum_per_odpair = data.groupby(['ORIGZONENO', 'DESTZONENO'])['ODTRIPS'].sum().to_frame().reset_index()
+filter_3 = trip_sum_per_odpair['DESTZONENO'] == 3003190
+ 
+#print trip_sum_per_odpair[filter_3]
+ 
+#===============================================================================
+#    ORIGZONENO  DESTZONENO  ODTRIPS
+# 0     3004925     3003190     11.0
+#===============================================================================
+ 
+total_merged_df = unique_route_sum.merge(trip_sum_per_odpair, on = ['ORIGZONENO', 'DESTZONENO'])
+change_col_total_merged = {'ODTRIPS_x' : 'ROUTE_OD',  'ODTRIPS_y':'TOTAL_OD' }
+total_merged_df = total_merged_df.rename(columns = change_col_total_merged)
+ 
+total_merged_df['SHARE(%)'] = (total_merged_df['ROUTE_OD'] / total_merged_df['TOTAL_OD'])*100
+ 
+ 
+filter_4 = total_merged_df['DESTZONENO'] == 3003190
+ 
+print total_merged_df[filter_4]
+ 
+save_resuls = "C:\\Users\\thenuwan.jayasinghe\\OneDrive - tum.de\\Thesis\\3_Analysis\\Direct Assignment\\sample_results_18022020.csv"
+  
+total_merged_df.to_csv(save_resuls)
+  
+total_merged_df.hist(column = 'SHARE(%)', bins = 100)
+plt.show()
 
 
 # 1. removing OD pairs < 10 trips
@@ -80,6 +79,7 @@ data_df = pd.read_csv("C:\\Users\\thenuwan.jayasinghe\\OneDrive - tum.de\\Thesis
 total_od_greater_10 = data_df['TOTAL_OD'] >= 10
 
 data_od_greater_10_df = data_df[total_od_greater_10]
+data_od_greater_10_df.to_csv("C:\\Users\\thenuwan.jayasinghe\\OneDrive - tum.de\\Thesis\\3_Analysis\\Direct Assignment\\df1.csv")
 #===============================================================================
 # data_od_greater_10_df['SHARE(%)'].hist(bins =100)
 # plt.show()
