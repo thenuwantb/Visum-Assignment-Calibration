f63d65b816c6344083a1049e4bbc569646208f55
thenuwan.jayasinghe <thenuwan.jayasinghe@gmail.com>
2020-02-20 19:47:28 +0800

    opened direct assignment version file from RP10 server

--- visum_calibration/implement_calibration_algorithm/singapore_network/min_route_share.py
+++ visum_calibration/implement_calibration_algorithm/singapore_network/min_route_share.py
@@ -4,3 +4,19 @@ Created on 20 Feb 2020
 @author: thenuwan.jayasinghe
 @note : iterate over all the origins of direct assignment visum file's PuT paths and calculate a value for the minimum route share %
 '''
+
+import sys
+import os
+import win32com.client as com
+import pandas as pd
+from custom_visum_functions.open_close_visum import open_close as ocv
+
+
+path = "E:\\Thenuwan\\DirectAssignment-10 days\\Visum Files"
+verFile = "OriginalNetwork_DirectAssignment.ver"
+versionPath = os.path.join(path, verFile)
+Visum = com.Dispatch("Visum.Visum.170")
+
+ocv.loadVisum(VisumComDispatch=Visum, verPath=versionPath)
+
+
