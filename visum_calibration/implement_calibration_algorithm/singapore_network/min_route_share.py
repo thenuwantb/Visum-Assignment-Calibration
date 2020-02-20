38a76f43b3d4920b5b661232d1cc00319fcf508b
thenuwan.jayasinghe <thenuwan.jayasinghe@CE-2013-126.TUMCREATE.LOCAL>
2020-02-20 15:07:10 +0800

    note of the script updated

--- visum_calibration/implement_calibration_algorithm/singapore_network/min_route_share.py
+++ visum_calibration/implement_calibration_algorithm/singapore_network/min_route_share.py
@@ -1,1 +1,6 @@
-#test thenuwan
+'''
+Created on 20 Feb 2020
+
+@author: thenuwan.jayasinghe
+@note : iterate over all the origins of direct assignment visum file's PuT paths and calculate a value for the minimum route share %
+'''
